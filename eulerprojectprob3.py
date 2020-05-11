da917cc332d945e5e188c5dd176242c2ee61c879
Ege Boz <56655376+bozege@users.noreply.github.com>
2020-05-11 14:46:52 +0300

    Update eulerprojectprob3.py

--- eulerprojectprob3.py
+++ eulerprojectprob3.py
@@ -1,27 +1,36 @@
 #Prime factor calculator
 
-num = int(input("Give me a number: "))
-sqrt = int((num**(1/2))//1)
-list = []
-k = 1
+def primefactors():
+    
+    
+    num = int(input("Give me a number: "))
+    sqrt = int((num**(1/2))//1)
+    list = []
+    k = 1
+    
+    if num == 1:
+        list.append(num)
+    
+    for i in range (2,sqrt+1):
+        while num % i == 0:
+            k = i
+            num = int(num / i)
+            list.append(k)
+            
+    if num > sqrt:
+        list.append(num)
+    
+    if list[0] == 1 and len(list) != 1:
+        list.pop(0)
+    
+    return (list)
+print("The prime factors of that number: ",primefactors())
+    
+    
+    
+    
+    
 
-if num == 1:
-    list.append(num)
-
-for i in range (2,sqrt+1):
-    while num % i == 0:
-        k = i
-        num = int(num / i)
-        list.append(k)
-        
-if num > sqrt:
-    list.append(num)
-
-if list[0] == 1 and len(list) != 1:
-    list.pop(0)
-
-
-print("The prime factors of that number: ",list)
 
 
 
