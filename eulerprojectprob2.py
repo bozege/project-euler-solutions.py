e4e5368a2ec9821baa0db6c40981a9785ed59dcc
Ege Boz <56655376+bozege@users.noreply.github.com>
2020-05-11 03:53:07 +0300

    Update eulerprojectprob2.py

--- eulerprojectprob2.py
+++ eulerprojectprob2.py
@@ -1,17 +1,23 @@
-#sum of even fibonacci numbers whose values do not exceed four million.
+#sum of even fibonacci numbers below four million.
 
-fib1 = 1
-fib2 = 2
-fib3 = fib1 + fib2
-
-sum = 2
-
-while fib3 <= 4000000:
-    if fib3 % 2 == 0:
-        sum += fib3
-    fib1 = fib2
-    fib2 = fib3
+def fibonacci(n):
+    
+    fib1 = 1
+    fib2 = 2
     fib3 = fib1 + fib2
-print(sum)
+    fiblist = [1,2]
+    while fib3 < n:
+        fiblist.extend([fib3])
+        fib1 = fib2
+        fib2 = fib3
+        fib3 = fib1 + fib2
+    
+    return(fiblist)
+
+sum = 0
+for i in range (0,len(fibonacci(4000000))):
+    if fibonacci(4000000)[i] % 2 == 0:
+        sum += fibonacci(4000000)[i]
+print("Sum of all even fibonacci numbers until that number is: ", sum)
         
     
